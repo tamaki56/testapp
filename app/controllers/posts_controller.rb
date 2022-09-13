0b21f955fddc4f6059a143866524c11949007cec
tamaki56 <http56@icloud.com>
2022-09-13 22:43:08 +0000

    add post index.html edit,destroy button

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,12 @@
 class PostsController < ApplicationController
-  
+
   def index
     @posts = Post.all
-    @post = @posts.page(params[:page]).per(10) #ページネーション
+    @page = @posts.page(params[:page]).per(10) #ページネーション
+  end
+  
+  def show
+    @post = Post.find(params[:id])
   end
 
   def edit
@@ -16,24 +20,28 @@ class PostsController < ApplicationController
   def create
     @post = Post.new(post_params)
     @post.save
-    redirect_to posts_path
+    redirect_to post_path
   end
-  
+
   def update
     @post = Post.find(params[:id])
-    @post.update(post_params)
-    redirect_to posts_path
+    if @post.update(post_params)
+      flash[:notice] = "You have updated book successfully."
+      redirect_to posts_path
+    else
+      render :edit
+    end
   end
-  
+
   def destroy
     @post = Post.find(params[:id])
     @post.destroy
       redirect_to posts_path
-  end  
-  
+  end
+
   private
-    
+
   def post_params
     params.require(:post).permit(:name, :body)
-  end  
+  end
 end
