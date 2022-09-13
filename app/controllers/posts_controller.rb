e4e5a7b4824f109d930041b02cb637b518c6d00b
tamaki56 <http56@icloud.com>
2022-09-13 23:06:42 +0000

    add post_controller,order id DESC

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
 
   def index
-    @posts = Post.all
+    @posts = Post.all.order(id: "DESC")
     @page = @posts.page(params[:page]).per(10) #ページネーション
   end
   
