2f88c3cb8c9c6a61843f966bc278f3c8cfd86e04
tamaki56 <http56@icloud.com>
2022-09-13 20:58:11 +0000

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  get 'posts/new'
-  get 'posts/index'
-  get 'posts/edit'
-  get 'homes/top'
+  root to: 'homes#top'
+  resources :posts, only: [:index,:edit,:new,:create,:update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
