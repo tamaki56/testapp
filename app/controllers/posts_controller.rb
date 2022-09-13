3fe552d116eeab7086b7312708767756d7bb938f
tamaki56 <http56@icloud.com>
2022-09-13 22:49:45 +0000

    add post edit.html

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -20,7 +20,7 @@ class PostsController < ApplicationController
   def create
     @post = Post.new(post_params)
     @post.save
-    redirect_to post_path
+    redirect_to posts_path
   end
 
   def update
