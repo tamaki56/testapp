eb573643578a551f9cef09ea664d122bac1a7bb0
tamaki56 <http56@icloud.com>
2022-09-13 21:11:08 +0000

    add post conntroller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,39 @@
 class PostsController < ApplicationController
-  def new
-  end
-
+  
   def index
+    @posts = Post.all
+    @post = @posts.page(params[:page]).per(10) #ページネーション
   end
 
   def edit
+    @post = Post.find(params[:id])
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.save
+    redirect_to posts_path
+  end
+  
+  def update
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+    redirect_to posts_path
+  end
+  
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+      redirect_to posts_path
+  end  
+  
+  private
+    
+  def post_params
+    params.require(:post).permit(:name, :body)
+  end  
 end
